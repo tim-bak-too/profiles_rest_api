800f0b962bdc5b7f3454c2e7ab18ee096d0f3849
srini <raju.g.srinivas@gmail.com>
2020-05-08 13:30:17 -0400

    Add HelloViewSet.

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -1,8 +1,14 @@
-from django.urls import path
+# profiles_api/urls.py
 
-from .views import HelloApiView
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 
+from .views import HelloApiView, HelloViewSet
+
+router = DefaultRouter()
+router.register('hello-viewset', HelloViewSet, base_name='hello-viewset')
 
 urlpatterns = [
     path('hello-view', HelloApiView.as_view()),
+    path('', include(router.urls)),
 ]
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,4 +1,5 @@
 from rest_framework.views import APIView
+from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import status
 
@@ -29,3 +30,32 @@ class HelloApiView(APIView):
 
     def delete(self, request, pk=None):
         return Response({'message': 'DELETE'})
+
+
+class HelloViewSet(ViewSet):
+    serializer_class = HelloSerializer
+
+    def list(self, request):
+        cities = ['Toronto', 'Ottawa', 'Bangalore']
+        return Response({'message': 'Hello!', 'cities': cities})
+
+    def create(self, request):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello, {name}'
+            return Response({'message': message})
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def retrieve(self, request, pk=None):
+        return Response({'http_method': 'GET'})
+
+    def update(self, request, pk=None):
+        return Response({'http_method': 'PUT'})
+
+    def partial_update(self, request, pk=None):
+        return Response({'http_method': 'PATCH'})
+
+    def destroy(self, request, pk=None):
+        return Response({'http_method': 'DELETE'})
