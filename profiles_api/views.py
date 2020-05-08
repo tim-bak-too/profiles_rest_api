61b48c186ede19b8e77e421c011f4d77935aa693
srini <raju.g.srinivas@gmail.com>
2020-05-08 09:50:21 -0400

    Add HelloApiView.

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class HelloApiView(APIView):
+    def get(self, request, format=None):
+        cities = ['Toronto', 'Ottawa', 'Bangalore']
+        return Response({'message': 'hello', 'cities': cities})
+    
