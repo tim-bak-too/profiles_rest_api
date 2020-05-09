dfc78fb2a7c9500c22c4cb83ecb24a1bde3de3a5
srini <raju.g.srinivas@gmail.com>
2020-05-09 10:55:35 -0400

    bug fix

--- profiles_api/permissions.py
+++ profiles_api/permissions.py
@@ -9,4 +9,4 @@ class UpdateOwnProfile(permissions.BasePermission):
         if request.method in permissions.SAFE_METHODS:
             return True
         else:
-            return obj.id == request.user_id
+            return obj.id == request.user.id
